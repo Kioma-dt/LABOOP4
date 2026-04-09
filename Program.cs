b0802d2449ceb5e52d18aba84c580368a2c4805d
Рома Авраменко <www.kioma220@gmail.com>
2026-04-09 16:39:18 +0300

    Ui Done

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ namespace LABOOP4
                         if (catalogFile is null) throw new Exception("Inputed Null String");
 
 
-                        if (catalogFile == "Def")
+                        if (catalogFile.ToLower() == "def")
                         {
                             catalogService = new JsonCatalogService();
                             cargoCatalog = catalogService.GetCargoCatalog("Files//catalog.json");
@@ -80,7 +80,7 @@ namespace LABOOP4
                         Console.WriteLine("Input Cargo Batch Name: ");
                         var cargoName = Console.ReadLine();
 
-                        if (cargoName == "Def")
+                        if (cargoName.ToLower() == "def")
                         {
                             cargoBatches = new List<(string, int)>
                                 {
@@ -222,26 +222,366 @@ namespace LABOOP4
 
                 logisticSystem.RegisterOrder(cargoBatches, distance, transportType, transportName);
 
+                IOrderSort? sort = null;
+                OrdersFilter filter = new OrdersFilter();
 
-                //    //IOrderSort sort = new DeliveryCostSort();
-                //    OrdersFilter filter = new OrdersFilter();
-                //    filter.AddFilter(new TransportTypeFilter(TransportType.Air));
+                Console.WriteLine("Press S to Add Sort");
+                var sortAnswer = Console.ReadLine();
 
+                if (sortAnswer.ToLower() == "s")
+                {
+                    bool sortContinue = true;
+                    while (sortContinue)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("1. Delivery Cost Sort");
+                        Console.WriteLine("2. Delivery Time Sort");
+                        Console.WriteLine("3. Transport Type Sort");
+                        Console.WriteLine("4. Transport Name Sort");
+                        var answer = Console.ReadLine();
+
+                        switch (answer)
+                        {
+                            case "1":
+                            {
+                                    sortContinue = false;
+                                    sort = new DeliveryCostSort();
+                                    break;
+                            }
+                            case "2":
+                                {
+                                    sortContinue = false;
+                                    sort = new DeliveryTimeSort();
+                                    break;
+                                }
+                            case "3":
+                                {
+                                    sortContinue = false;
+                                    sort = new TransportTypeSort();
+                                    break;
+                                }
+                            case "4":
+                                {
+                                    sortContinue = false;
+                                    sort = new TransportNameSort();
+                                    break;
+                                }
+                            default:
+                                {
+                                    sortContinue = true;
+                                    Console.WriteLine("Wrong Option Format!\nPress Any Key...");
+                                    Console.ReadLine();
+                                    Console.Clear();
+                                    break;
+                                }
+                        }
+                    }
+                }
+
+                var continueFilter = true;
+
+                while (continueFilter) 
+                {
+                    Console.Clear();
+                    Console.WriteLine("Press F to add Filter");
+                    var filterAnwer = Console.ReadLine();
+
+                    if (filterAnwer.ToLower() != "f") 
+                    {
+                        continueFilter = false;
+                        break;
+                    }
+
+                    var continueChoose = true;
+                    var filterName = String.Empty;
+                    while (continueChoose)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("1. Delivery Cost Filter");
+                        Console.WriteLine("2. Delivery Time Filrer");
+                        Console.WriteLine("3. Transport Type Filter");
+                        var answer = Console.ReadLine();
+
+
+
+                        switch (answer)
+                        {
+                            case "1":
+                                {
+                                    continueChoose = false;
+                                    filterName = "Cost";
+                                    break;
+                                }
+                            case "2":
+                                {
+                                    continueChoose = false;
+                                    filterName = "Time";
+                                    break;
+                                }
+                            case "3":
+                                {
+                                    continueChoose = false;
+                                    filterName = "Type";
+                                    break;
+                                }
+                            default:
+                                {
+                                    continueChoose = true;
+                                    Console.WriteLine("Wrong Option Format!\nPress Any Key...");
+                                    Console.ReadLine();
+                                    Console.Clear();
+                                    break;
+                                }
+                        }
+
+                    }
+
+                    if (filterName == "Type")
+                    {
+                        continueChoose = true;
+                        while (continueChoose) 
+                        {
+                            Console.Clear();
+                            Console.WriteLine("1. Land");
+                            Console.WriteLine("2. Air");
+                            Console.WriteLine("3. Water");
+                            var answer = Console.ReadLine();
+
+
+
+                            switch (answer)
+                            {
+                                case "1":
+                                    {
+                                        continueChoose = false;
+                                        filter.AddFilter(new TransportTypeFilter(TransportType.Land));
+                                        break;
+                                    }
+                                case "2":
+                                    {
+                                        continueChoose = false;
+                                        filter.AddFilter(new TransportTypeFilter(TransportType.Air));
+                                        break;
+                                    }
+                                case "3":
+                                    {
+                                        continueChoose = false;
+                                        filter.AddFilter(new TransportTypeFilter(TransportType.Water));
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        continueChoose = true;
+                                        Console.WriteLine("Wrong Option Format!\nPress Any Key...");
+                                        Console.ReadLine();
+                                        Console.Clear();
+                                        break;
+                                    }
+                            }
+                        }
+                        
+                    }
+                    else
+                    {
+                        var compType = ComparisonType.Greater;
+                        continueChoose = true;
+                        while (continueChoose)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("1. Greater");
+                            Console.WriteLine("2. Greater Or Equal");
+                            Console.WriteLine("3. Equal");
+                            Console.WriteLine("4. Less Or Equal");
+                            Console.WriteLine("5. Less");
+                            var answer = Console.ReadLine();
+
+
+
+                            switch (answer)
+                            {
+                                case "1":
+                                    {
+                                        continueChoose = false;
+                                        compType = ComparisonType.Greater;
+                                        break;
+                                    }
+                                case "2":
+                                    {
+                                        continueChoose = false;
+                                        compType = ComparisonType.GreaterOrEqual;
+                                        break;
+                                    }
+                                case "3":
+                                    {
+                                        continueChoose = false;
+                                        compType = ComparisonType.Equal;
+                                        break;
+                                    }
+                                case "4":
+                                    {
+                                        continueChoose = false;
+                                        compType = ComparisonType.LessOrEqual;
+                                        break;
+                                    }
+                                case "5":
+                                    {
+                                        continueChoose = false;
+                                        compType = ComparisonType.Less;
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        continueChoose = true;
+                                        Console.WriteLine("Wrong Option Format!\nPress Any Key...");
+                                        Console.ReadLine();
+                                        Console.Clear();
+                                        break;
+                                    }
+                            }
+                        }
+
+                        continueChoose = true;
+                        double value = 0;
+                        while (continueChoose)
+                        {
+                            try
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Input Compare Value: ");
+                                value = Double.Parse(Console.ReadLine() ?? String.Empty);
+                                continueChoose = false;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine($"Distance Error: Distance Should Be Pos Int\nPress Any Key...");
+                                Console.ReadLine();
+                                Console.Clear();
+                                continueChoose = true;
+                            }
+                            
+                        }
+
+                        if (filterName == "Cost")
+                        {
+                            filter.AddFilter(new DeliveryCostFilter(value, compType));
+                        }
+                        else
+                        {
+                            filter.AddFilter(new DeliveryTimeFilter(value, compType));
+                        }
+                    }
+
+                }
+
+                Console.Clear();
                 var orders = logisticSystem.Orders;
-                //    orders = filter.ApplyFilters(orders);
-                //    //orders = sort.Sort(orders);
-                //    var saveDTOs = new List<SaveDTO>();
+                orders = filter.ApplyFilters(orders);
+                orders = sort?.Sort(orders) ?? orders;
+                var saveDTOs = new List<SaveDTO>();
                 foreach (var order in orders)
                 {
-                    Console.WriteLine($"Стоимость: {order.GetCost():F2}");
-                    Console.WriteLine($"Время доставки: {order.GetDeliveryTime():F2} часов");
+                    Console.WriteLine($"Transport Name: {order.TrasnportName}");
+                    Console.WriteLine($"Transport Type: {order.TrasnportType}");
+                    Console.WriteLine($"Delivery Cost: {order.GetCost():F2}");
+                    Console.WriteLine($"Delivery Time: {order.GetDeliveryTime():F2} hours");
                     Console.WriteLine();
-                    //saveDTOs.Add(new SaveDTO(order));
+                    saveDTOs.Add(new SaveDTO(order));
                 }
-                //    SaveService saveService = new CsvSaveService();
-                //    //saveService = new EncryptionDecorator(saveService, key, iv);
-                //    //saveService = new ZipDecorator(saveService, "Files//archive.zip");
-                //    saveService.Save(saveDTOs, "Files//output.csv");
+                Console.WriteLine("Press Any Key...");
+                Console.ReadLine();
+
+
+
+                SaveService? saveService = null;
+                string? saveFile = null;
+                //    //
+                //    
+                //   
+
+                while (saveService is null)
+                {
+                    try
+                    {
+                        Console.WriteLine("Input Save File: ");
+                        saveFile = Console.ReadLine();
+
+
+                        if (saveFile is null) throw new Exception("Inputed Null String");
+
+
+                        if (saveFile.ToLower() == "def")
+                        {
+                            saveService = new JsonSaveService();
+                            saveFile = "Files//output.json";
+                            break;
+                        }
+
+                        if (saveFile.EndsWith(".json")) saveService = new JsonSaveService();
+                        else if (saveFile.EndsWith(".csv")) saveService = new CsvSaveService();
+                        else throw new Exception("Invalid Save File Format");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Save File Error: {ex.Message}\nPress Any Key...");
+                        saveService = null;
+                        saveFile = null;
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+
+                }
+
+                Console.Clear();
+                Console.WriteLine("Press E to Encrypt");
+                var encAnswer = Console.ReadLine();
+
+                if (encAnswer.ToLower() == "e") 
+                {
+                    saveService = new EncryptionDecorator(saveService, key, iv);
+                }
+
+                Console.Clear();
+                Console.WriteLine("Press Z to Zip");
+                var zipAnswer = Console.ReadLine();
+
+                if (zipAnswer.ToLower() == "z")
+                {
+                    string? zipFile = null;
+                    while (zipFile is null)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Input Zip File: ");
+                            zipFile = Console.ReadLine();
+
+
+                            if (zipFile is null) throw new Exception("Inputed Null String");
+
+
+                            if (zipFile.ToLower() == "def")
+                            {
+                                zipFile = "Files//archive.zip";
+                                break;
+                            }
+
+                            if (!zipFile.EndsWith(".zip")) throw new Exception("Invalid Zip File Format");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Zip File Error: {ex.Message}\nPress Any Key...");
+                            zipFile = null;
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+
+                    }
+
+                    saveService = new ZipDecorator(saveService, zipFile);
+                }
+
+                saveService.Save(saveDTOs, saveFile);
+                Console.Clear();
+                Console.WriteLine("Saved!!!");
             }
             catch (Exception ex)
             {
@@ -250,15 +590,3 @@ namespace LABOOP4
         }
     }
 }
-
-
-//    //logisticSystem.RegisterOrder(
-//    //    new List<(string, int)>
-//    //    {
-//    //        ("Электроника", 10),
-//    //        ("Одежда", 50)
-//    //    },
-//    //    500,
-//    //    TransportType.Land,
-//    //    "Грузовик"
-//    //);
